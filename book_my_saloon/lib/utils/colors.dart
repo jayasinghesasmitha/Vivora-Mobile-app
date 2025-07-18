d12972e4ba043b00ea214bdbaa3b46c453371016
Sasmitha Uvindu Jayasinghe <158791024+jayasinghesasmitha@users.noreply.github.com>
2025-07-18 16:36:53 +0530

    Create colors.dart

---
+++ book_my_saloon/lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF6A1B9A);
+  static const Color secondaryColor = Color(0xFF9C27B0);
+  static const Color accentColor = Color(0xFFE91E63);
+  static const Color textColor = Color(0xFF333333);
+  static const Color lightTextColor = Color(0xFF757575);
+  static const Color backgroundColor = Color(0xFFF5F5F5);
+}
