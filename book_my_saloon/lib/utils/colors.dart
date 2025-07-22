d3608110d405825cb5878b5a31813766f06801a3
Sasmitha Uvindu Jayasinghe <158791024+jayasinghesasmitha@users.noreply.github.com>
2025-07-22 22:42:00 +0530

    Update colors.dart

--- book_my_saloon/lib/utils/colors.dart
+++ book_my_saloon/lib/utils/colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF6A1B9A);
-  static const Color secondaryColor = Color(0xFF9C27B0);
-  static const Color accentColor = Color(0xFFE91E63);
-  static const Color textColor = Color(0xFF333333);
-  static const Color lightTextColor = Color(0xFF757575);
+  static const Color primaryColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color secondaryColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color accentColor = Color.fromARGB(255, 255, 0, 85);
+  static const Color textColor = Color.fromARGB(255, 0, 0, 0);
+  static const Color lightTextColor = Color.fromARGB(255, 0, 0, 0);
   static const Color backgroundColor = Color(0xFFF5F5F5);
 }
