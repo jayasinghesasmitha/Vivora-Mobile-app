81d98ba929522af6f44bc14b35fbc901c4279899
Sasmitha Uvindu Jayasinghe <158791024+jayasinghesasmitha@users.noreply.github.com>
2025-07-29 23:33:17 +0530

    Update main.dart

--- book_my_saloon/lib/main.dart
+++ book_my_saloon/lib/main.dart
@@ -63,7 +63,6 @@ class MyApp extends StatelessWidget {
 */
 
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:book_my_saloon/screens/start_screen.dart'; // Import StartScreen
 import 'package:book_my_saloon/services/auth_service.dart';
@@ -72,14 +71,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  await Firebase.initializeApp(
-    options: FirebaseOptions(
-      apiKey: dotenv.env['API_KEY'] ?? '',
-      appId: dotenv.env['APP_ID'] ?? '',
-      messagingSenderId: dotenv.env['MESSAGING_SENDER_ID'] ?? '',
-      projectId: dotenv.env['PROJECT_ID'] ?? '',
-    ),
-  );
   runApp(const MyApp());
 }
 
